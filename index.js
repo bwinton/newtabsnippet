ee5f0dbd89a6acb3d1d0cb85a8bc8204f88c0db2
Blake Winton <bwinton@latte.ca>
2015-06-24 11:52:58 -0700

    Clean up debugging logs, and add UITelemetry support.

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 var PrefSvc = require('sdk/preferences/service');
 // var tabs = require('sdk/tabs');
 
+var { Cu } = require('chrome');
+Cu.import('resource://gre/modules/UITelemetry.jsm');
+
+var telemetry = {};
+
 exports.main = function () {
   let previous = PrefSvc.get('browser.newtab.preload');
-  console.log("1", previous, typeof previous);
   PrefSvc.set('browser.newtab.preload', false);
   PrefSvc.set('browser.newtab.preload.previous', previous);
+
+  if (UITelemetry.enabled) {
+    UITelemetry.removeSimpleMeasureFunction('newtabsnippets');
+    UITelemetry.addSimpleMeasureFunction('newtabsnippets', function () {
+      return telemetry;
+    });
+  }
 };
 
-exports.onUnload = function () {
+exports.onUnload = function (reason) {
   // tabs.removeListener('ready', tabReady);
   let previous = PrefSvc.get('browser.newtab.preload.previous');
-  console.log("2", previous, typeof previous)
   PrefSvc.set('browser.newtab.preload', previous);
   PrefSvc.reset('browser.newtab.preload.previous');
+
+  if (UITelemetry.enabled && reason !== 'shutdown') {
+    UITelemetry.removeSimpleMeasureFunction('newtabsnippets');
+  }
 };
